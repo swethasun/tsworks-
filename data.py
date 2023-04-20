72bc0a57821901b58e6f87cf043ba9bfa19e607a
swethasun <130659017+swethasun@users.noreply.github.com>
2023-04-20 15:50:14 +0530

    Add files via upload
    url of other companies can be used

---
+++ data.py
@@ -0,0 +1,14 @@
+url = 'https://finance.yahoo.com/quote/IBM/history?p=IBM'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table', {'data-test': 'historical-prices'})
+data = []
+
+for row in table.tbody.find_all('tr'):
+    cols = row.find_all('td')
+    cols = [col.text.strip() for col in cols]
+    data.append(cols)
+
+df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
